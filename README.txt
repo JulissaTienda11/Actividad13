52f9741d2bb2384a3262ed8c6bafcac5a933bd04
Julissa Tienda <160643018+JulissaTienda11@users.noreply.github.com>
2024-06-12 16:15:04 +0000

    Initial commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+DE: a, b
+DS: c
+
+1. Inicio
+2. Leer a
+3. Leer b
+4. Hacer a-b 
+5. Mostrar c
+6. Fin
---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Operación Algebraica</title>
+  </head>
+  <body>
+    <?php
+      $a = 15;
+      $b = 5;
+      $c =$a-$b;
+      echo $c;
+    ?> 
+  </body>
+</html>
